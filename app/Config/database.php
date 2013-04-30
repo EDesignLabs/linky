dd022e5ead86332e33887f3d2a792f460492a5e5
Pritika Nilaratna <pritika.16@gmail.com>
2013-04-29 21:55:50 -0400

    Added composer.json for installing vendor files. Ignoring vendor folder.

--- app/Config/database.php
+++ app/Config/database.php
@@ -64,7 +64,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'root',
 		'database' => 'themeboard',
 		'prefix' => '',
 		//'encoding' => 'utf8',
