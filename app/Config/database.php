12454e46567b3514ba7136647fff4c93d2513799
Pritika Nilaratna <pritika.16@gmail.com>
2013-05-10 13:51:26 -0400

    Added datasource
    Signed-off-by: Pritika Nilaratna <pritika.16@gmail.com>

--- app/Config/database.php
+++ app/Config/database.php
@@ -61,9 +61,8 @@ class DATABASE_CONFIG {
 
 	function __construct() {
         $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-        var_dump($url);
         $this->default = array(
-            'driver' => 'mysql',
+            'datasource' => 'Database/MySql',
             'host' => $url['host'],
             'login' => $url['user'],
             'password' => $url['pass'],
